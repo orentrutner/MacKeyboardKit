18187a87b1e397cb2549176ef180662e4c85d0aa
Oren Trutner <orentrutner@gmail.com>
2017-11-19 15:29:24 -0800

    Initial Commit

---
+++ MacKeyboardKit/MacKeyboardKit.h
@@ -0,0 +1,19 @@
+//
+//  MacKeyboardKit.h
+//  MacKeyboardKit
+//
+//  Created by Oren Trutner on 11/19/17.
+//  Copyright © 2017 Oren Trutner. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for MacKeyboardKit.
+FOUNDATION_EXPORT double MacKeyboardKitVersionNumber;
+
+//! Project version string for MacKeyboardKit.
+FOUNDATION_EXPORT const unsigned char MacKeyboardKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MacKeyboardKit/PublicHeader.h>
+
+
